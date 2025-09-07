e02bdc3e99a7a7a87d48caee54f6cad0cae4aa48
acmathwiz <adriancmathwiz@gmail.com>
2025-09-06 17:35:30 -0700

    Add intro

---
+++ WordOccurrences.py
@@ -0,0 +1,3 @@
+# You are given a list of words. Some words may repeat. For each word, output the number of times it occurs.
+#
+# The order of output should correspond to the order of first appearance of each word in the input.
